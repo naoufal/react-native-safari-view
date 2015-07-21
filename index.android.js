bee07b93e28fa60cc7e99242bffd933ea5567b64
Naoufal Kadhom <naoufalkadhom@gmail.com>
2015-07-21 08:55:01 -0400

    lint fixes

--- index.android.js
+++ index.android.js
@@ -10,7 +10,7 @@ var warning = require('warning');
 
 var SafariViewManager = {
   test: function() {
-    warning("Not yet implemented for Android.");
+    warning('Not yet implemented for Android.');
   }
 };
 
