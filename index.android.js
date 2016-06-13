7091474e66355cf0ece14c28a8157d626ae9c3d8
Mohit Sharma <mohits@swiftsetup.com>
2016-06-13 11:47:29 +0530

    Fixing lint issues

--- index.android.js
+++ index.android.js
@@ -7,14 +7,22 @@
 'use strict';
 
 var warning = require('warning');
+const {NativeModules} = require('react-native');
+const NativeSafariViewManager = NativeModules.SafariViewManager;
 
 var SafariViewManager = {
   test: function() {
     warning('Not yet implemented for Android.');
   },
-  isAvailable() {
+  isAvailable: function() {
     return new Promise(function(resolve, reject) {
+      NativeSafariViewManager.isAvailable(function(error) {
+        if (error) {
+          return reject(error);
+        }
+
         resolve(false);
+      });
     });
   }
 };
