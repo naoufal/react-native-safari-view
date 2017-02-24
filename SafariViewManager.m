902d35960257acbe0b63959f88d2a2afe3abc67b
seasonyuu <seasonyuu@gmail.com>
2017-02-24 17:22:29 +0800

    Fix readerMode bug naoufal/react-native-safari-view#56

--- SafariViewManager.m
+++ SafariViewManager.m
@@ -28,7 +28,7 @@ RCT_EXPORT_METHOD(show:(NSDictionary *)args callback:(RCTResponseSenderBlock)cal
     }
 
     // Initialize the Safari View
-    self.safariView = [[SFSafariViewController alloc] initWithURL:[NSURL URLWithString:args[@"url"]] entersReaderIfAvailable:args[@"readerMode"]];
+    self.safariView = [[SFSafariViewController alloc] initWithURL:[NSURL URLWithString:args[@"url"]] entersReaderIfAvailable:[args[@"readerMode"] boolValue]];
     self.safariView.delegate = self;
 
     // Set tintColor if available
